8e5ad08f096718d860b8a86964cc0a297049814d
rahul6975 <rahulya7569@gmail.com>
2021-07-18 17:38:36 +0530

    animations

--- food_maniacs/lib/animation/RotationRoute.dart
+++ food_maniacs/lib/animation/RotationRoute.dart
@@ -7,28 +7,28 @@ class RotationRoute extends PageRouteBuilder {
       : super(
           pageBuilder: (
             BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-          ) =>
-              page,
-          transitionDuration: Duration(seconds: 1),
-          transitionsBuilder: (
-            BuildContext context,
-            Animation<double> animation,
-            Animation<double> secondaryAnimation,
-            Widget child,
-          ) =>
-          //     RotationTransition(
-          //   turns: Tween<double>(
-          //     begin: 0.0,
-          //     end: 1.0,
-          //   ).animate(
-          //     CurvedAnimation(
-          //       parent: animation,
-          //       curve: Curves.linear,
-          //     ),
-          //   ),
-          //   child: child,
-          // ),
+          //   Animation<double> animation,
+          //   Animation<double> secondaryAnimation,
+          // ) =>
+          //     page,
+          // transitionDuration: Duration(seconds: 1),
+          // transitionsBuilder: (
+          //   BuildContext context,
+          //   Animation<double> animation,
+          //   Animation<double> secondaryAnimation,
+          //   Widget child,
+          // ) =>
+              RotationTransition(
+            turns: Tween<double>(
+              begin: 0.0,
+              end: 1.0,
+            ).animate(
+              CurvedAnimation(
+                parent: animation,
+                curve: Curves.linear,
+              ),
+            ),
+            child: child,
+          ),
         );
 }
