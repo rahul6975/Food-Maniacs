47ac1519c1a6e46d5e96cbdbdf74349a07c8110a
rahul6975 <rahulya7569@gmail.com>
2021-07-18 17:34:59 +0530

    animations

--- food_maniacs/lib/animation/RotationRoute.dart
+++ food_maniacs/lib/animation/RotationRoute.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class RotationRoute extends StatelessWidget {
   RotationRoute({Key key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Container(
